00160bfe4c2703e21ace5130e825d7f160869350
akhilak9 <akhilaakkik9@gmail.com>
2021-04-06 23:04:36 +0530

    ang commit

---
+++ Ang-T05-HOL_002/template-form-proj/src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+
+import { addressModel } from 'src/app/data/address.model';
+
+@Component({
+  selector: "app-template-driven-form",
+
+  templateUrl: "./template-driven-form.component.html",
+
+  styleUrls: ["./template-driven-form.component.css"],
+})
+export class TemplateDrivenFormComponent implements OnInit {
+  countryData: any[] = ["India", "US", "UK"];
+
+  model: addressModel = {
+    address: "",
+
+    city: "",
+
+    state: "",
+
+    postcode: null,
+
+    country: null,
+
+    aggrement: false,
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onFormSubmit() {
+    console.log("Full Address", this.model);
+  }
+}
+
